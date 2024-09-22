ed81affcdfe0b6b8ee6f57ac5c4153970c70d4b2
Vinicius Pereira <130008311+0bvim@users.noreply.github.com>
2024-09-22 13:07:50 -0300

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,64 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- my coolnight colorscheme
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 12
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.77
+config.window_background_image = "/home/vinicius/Pictures/black-wallpaper.jpeg"
+
+-- set default terminal
+config.default_prog = { "/usr/bin/zsh" }
+config.text_background_opacity = 0.81
+
+-- Keybinding for maximizing/restoring the active pane
+config.keys = {
+	{
+		key = "X",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	-- Vim-like pane navigation with Ctrl + Shift + h/j/k/l
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+}
+
+-- Finally, return the configuration to wezterm
+return config
